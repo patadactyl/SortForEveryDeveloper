a8bcd21206d8f0c7e858897c855f284a81785ad4
Patrick Charlton <patrick.charlton6@gmail.com>
2019-12-22 13:38:36 -0800

    Added a while loop to run the methods continuously.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,51 +6,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int MAX_CHAR = 26;
-
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String string = scanner.nextLine();
-
-        System.out.println("String sorted in alphabetical order: ");
-        alphabeticalSort(string);
-
-
-        System.out.println("String sorted in reverse alphabetical order: ");
-        reverseAlphabeticalSort(string);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a new string of alphabetical characters (ctrl+D to exit): ");
+            String userInputString = scanner.nextLine();
+            System.out.println("String sorted in alphabetical order: ");
+            alphabeticalSort(userInputString);
+            System.out.println("String sorted in reverse alphabetical order: ");
+            reverseAlphabeticalSort(userInputString);
+        }
     }
 
     public static void alphabeticalSort(String userInput) {
-
         Character[] charArray = new Character[userInput.length()];
-
         for (int i = 0; i < userInput.length(); i++) {
             charArray[i] = userInput.charAt(i);
         }
-
         Arrays.sort(charArray, Comparator.comparingInt(Character::toLowerCase));
-
         StringBuilder stringBuilder = new StringBuilder(charArray.length);
         for (Character character : charArray)
             stringBuilder.append(character.charValue());
-
         System.out.println(stringBuilder.toString());
     }
 
-    public static void reverseAlphabeticalSort(String userInput) {
-        int countingHashArray[] = new int[MAX_CHAR];
-
-        for (int i = 0; i < userInput.length(); i++) {
-            countingHashArray[userInput.charAt(i) - 'a']++;
+    public static void reverseAlphabeticalSort(String userInputReverse) {
+        char[] reverseCharacterArray = userInputReverse.toCharArray();
+        int length = reverseCharacterArray.length;
+        char tempChar;
+        for (int i = 0; i < length / 2; i++)
+        {
+            tempChar = reverseCharacterArray[i];
+            reverseCharacterArray[i] = reverseCharacterArray[length - i - 1];
+            reverseCharacterArray[length - i - 1] = tempChar;
         }
-
-        for (int i = MAX_CHAR - 1; i >= 0; i--) {
-            for (int j = 0; j < countingHashArray[i]; j++) {
-                System.out.print((char) ('a' + i));
-            }
-        }
+        System.out.println(userInputReverse);
     }
 }
 
